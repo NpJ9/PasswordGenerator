d3db951964ae3213dd3a3972ba1414571eb18960
Jack Naylor <jacknaylor999@hotmail.com>
2024-12-29 22:56:04 +0700

    refactored

--- password-generator-app/starter-code/script.js
+++ password-generator-app/starter-code/script.js
@@ -1,80 +1,107 @@
+let button = document.getElementById("generate");
+let slider = document.getElementById("slider");
+let numberChecked = document.getElementById("numbers");
+let lowerCaseChecked = document.getElementById("lowercase");
+let upperCaseChecked = document.getElementById("uppercase");
+let symbolsChecked = document.getElementById("symbols");
+let svgBtn = document.getElementById("svgBtn");
+let passwordLength = 10;
+let numbers ='1234567890'; 
+let lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+let UppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVQXYZ'
+let symbols = '~`!@#$%^&*()_-+={[}]|\:;"<,>.?/';
+let password = '';
+let strengthCounter = 0;
+
+button.addEventListener('click' , (e) =>{
+    check();
+    document.getElementById("paswordoutput").textContent = password;
+    password='';
+    strengthCounter = 0;
+});
+
+// Copy on svg click 
+svgBtn.addEventListener('click' , (e) =>{
+    let copied = document.getElementById("paswordoutput").textContent
+    navigator.clipboard.writeText(copied);
+});
+
+// Get slider input 
+
+document.getElementById("slider").oninput = function(){
+    passwordLength = document.getElementById("slider").value;
+    document.getElementById("passLength").innerHTML = passwordLength;
+    console.log("New password length " + passwordLength)
+}
+
+let check = () => {
+    if(document.getElementById('uppercase').checked === true)
+        {password = password.concat(UppercaseLetters)
+            strengthCounter = strengthCounter + 1};
+
+    if(document.getElementById('lowercase').checked === true) 
+        {password = password.concat(lowerCaseLetters)
+            strengthCounter = strengthCounter + 1}; 
+
+    if(document.getElementById('numbers').checked === true)
+        {password = password.concat(numbers)
+            strengthCounter = strengthCounter + 1};
+
+    if(document.getElementById('symbols').checked === true)
+        {password = password.concat(symbols)
+            strengthCounter = strengthCounter + 1};
+
+password = password.split('');
+
+// Fisher–Yates Shuffle
+
+    let currentIndex = password.length;
+    while(currentIndex !=0){
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [password[currentIndex], password[randomIndex]] =[
+            password[randomIndex], password[currentIndex]];
+    }
+
+    password = password.splice(0 , passwordLength).join('');
+    
+    console.log("Your password is: " + password);
+
+    // Change Display levels
+    
+    if (strengthCounter === 0){
+        document.getElementById("strength1").style.backgroundColor= 'white';
+        document.getElementById("strength2").style.backgroundColor= 'white';
+        document.getElementById("strength3").style.backgroundColor= 'white';   
+        document.getElementById("strength4").style.backgroundColor= 'white';
+        document.getElementById("strengthtext").textContent = "N/A"
+    } else if (strengthCounter === 1){
+        document.getElementById("strength1").style.backgroundColor= 'red';
+        document.getElementById("strength2").style.backgroundColor= 'white';
+        document.getElementById("strength3").style.backgroundColor= 'white';
+        document.getElementById("strength4").style.backgroundColor= 'white';
+        document.getElementById("strengthtext").textContent = "Weak"
+    } else if (strengthCounter === 2){
+        document.getElementById("strength1").style.backgroundColor= 'orange';
+        document.getElementById("strength2").style.backgroundColor= 'orange';
+        document.getElementById("strength3").style.backgroundColor= 'white';
+        document.getElementById("strength4").style.backgroundColor= 'white';
+        document.getElementById("strengthtext").textContent = "Ok"
+    } else if (strengthCounter === 3){
+        document.getElementById("strength1").style.backgroundColor= 'yellow';
+        document.getElementById("strength2").style.backgroundColor= 'yellow';
+        document.getElementById("strength3").style.backgroundColor= 'yellow';   
+        document.getElementById("strength4").style.backgroundColor= 'white';
+        document.getElementById("strengthtext").textContent = "Strong"
+    } else if (strengthCounter === 4) {
+        document.getElementById("strength1").style.backgroundColor= 'green';
+        document.getElementById("strength2").style.backgroundColor= 'green';
+        document.getElementById("strength3").style.backgroundColor= 'green';   
+        document.getElementById("strength4").style.backgroundColor= 'green';
+        document.getElementById("strengthtext").textContent = "Very Strong"
+    } 
+    
+}
 // TO DO
-
-// 1. 
-// Generate a password all in Uppercaseletters according to character length
-// Set character to length a default
-// Generate a randomized string of Uppercase characters
-
-// 2. 
-// Generate a password all in Lowercase according to character length
-// Set character to length a default
-// Generate a randomized string of Lowercase characters
-
-// 3. 
-// Generate a password all in numbers according to character length
-// Set character to length a default
-// Generate a randomized string of numbers characters
-
-// 4. 
-// Generate a password all in symbols according to character length
-// Set character to length a default
-// Generate a randomized string of symbols characters
-
-
-
-
-
-// Generates cryptographically strong random numbers
-// Better than Math.random as that is insecure   
-
-let getRandomNumber = () => {
-    let randomNumber = new Uint32Array(1); 
-    crypto.getRandomValues(randomNumber); 
-    console.log("Shuffled Number: " + randomNumber);
-}
-
-// Generate a randomized string of symbols characters
-
-let getRandomSymbols = () => {
-    let symbols = '~`!@#$%^&*()_-+={[}]|\:;"<,>.?/'
-    let randomSymbolsArray = symbols.split('');
-    let currentIndex = randomSymbolsArray.length;
-    
-    while(currentIndex !=0){
-        let randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [randomSymbolsArray[currentIndex], randomSymbolsArray[randomIndex]] =[
-            randomSymbolsArray[randomIndex], randomSymbolsArray[currentIndex]];
-    }
-
-    console.log("Shuffled symbols: " + randomSymbolsArray)
-    
-}
-
-// For shuffle
-// Fisher-Yates Shuffle 
-
-let getRandomLetters = ()=> {
-    let arr ='abcdefghijklmnopqrstuvwxyz';
-    let result = arr.split('');
-    let currentIndex = result.length;
-
-    while(currentIndex !=0){
-        let randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [result[currentIndex], result[randomIndex]] =[
-            result[randomIndex], result[currentIndex]];
-    }
-
-    console.log("shuffled alphabet: " + result.join(''));
-}
-
-
-getRandomNumber();
-getRandomSymbols();
-getRandomLetters();
-
-
-
+// Check the strength (how long/ how complex )
+// Make sure the password actually contains the added values (num?Upper)
